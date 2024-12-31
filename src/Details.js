ec91d7ebab13f0eb1f05a3132ad880c1832866d0
vidishachandra <vidishasateesh@gmail.com>
2024-12-30 20:17:42 -0800

    Updated portfolio details_5

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,10 @@
-//Steps to modify this code
-//npm install
+// Steps to modify this code
+// npm install
 // npm install gh-pages --save-dev
 // npm run build
 // npm run deploy
 // git init
-//git pull origin main
+// git pull origin main
 // git add .
 // git commit -m "Updated portfolio details"
 // git push origin main
@@ -149,23 +149,35 @@
 // };
 
 
+
+// Enter all your details in this file
 // Logo images
 import logogradient from "./assets/logo.svg";
 import logo from "./assets/logo2.svg";
-
 // Profile Image
 import profile from "./assets/profile.jpg";
-
 // Tech stack images
 import html from "./assets/techstack/html.png";
 import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
 import js from "./assets/techstack/js.png";
-import python from "./assets/techstack/python.png";
-import java from "./assets/techstack/java.png";
-import docker from "./assets/techstack/docker.png";
-import spark from "./assets/techstack/spark.png";
-import tableau from "./assets/techstack/tableau.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
 import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Project Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
 
 // Logos
 export const logos = {
@@ -176,45 +188,41 @@ export const logos = {
 // Enter your Personal Details here
 export const personalDetails = {
   name: "Vidisha Chandra",
-  tagline: "Data Engineer | Software Developer; Transforming data into innovation, one code at a time",
+  tagline: "Transforming data into innovation, one code at a time.",
   img: profile,
-  about: `I am a Master's student in Information Technology & Management at The University of Texas at Austin, with a strong foundation in computer science and a passion for building data-driven solutions. My experience spans developing scalable pipelines, optimizing systems, and creating innovative tools for unstructured and structured data analysis.`,
+  about: `A dedicated software engineer with a passion for solving complex problems and delivering impactful solutions. With hands-on experience in full-stack development and distributed systems, I am pursuing a Master of Science in Information Technology and Management at the University of Texas at Austin. I thrive in dynamic environments, leveraging my technical expertise and collaborative spirit to drive innovation and efficiency.`,
 };
 
-
+// Enter your Social Media URLs here
 export const socialMediaUrl = {
-  linkedin: "https://www.linkedin.com/in/vidisha-chandra", // Correct spelling
+  linkedin: "https://linkedin.com/in/vidisha-chandra",
   github: "https://github.com/vidishachandra",
   twitter: "https://twitter.com/",
   instagram: "https://www.instagram.com/",
 };
 
-
 // Enter your Work Experience here
 export const workDetails = [
   {
-    Position: "Data Engineer Intern",
+    Position: "Software Developer Intern",
     Company: "Dover Fueling Solutions",
     Location: "Austin, USA",
     Type: "Full Time",
     Duration: "Dec 2024 - Present",
-    Description: `Developed a centralized internal search tool to process and vectorize unstructured data, enhancing accessibility and reducing data retrieval time. Built an RAG pipeline for a query interface with a custom LLM, deployed as scalable APIs within Docker containers.`,
   },
   {
-    Position: "Data Engineer Intern",
+    Position: "Software Engineer Intern",
     Company: "Fidelity Investments",
     Location: "Bangalore, India",
     Type: "Internship",
-    Duration: "Jan - Jun 2024",
-    Description: `Automated corporate action processing with ETL business rules, transforming raw stock data into structured datasets. Innovated a Java module for dynamic data manipulation and optimized system performance for various data standards.`,
+    Duration: "Jan 2024 - Jun 2024",
   },
   {
-    Position: "Data Engineer Intern",
-    Company: "Fidelity Investments",
+    Position: "Full Stack Engineer Intern",
+    Company: "Cogvision.ai",
     Location: "Bangalore, India",
     Type: "Internship",
-    Duration: "Jun - Aug 2023",
-    Description: `Created a semantic search chatbot to deliver contextually relevant responses, improving accuracy and reducing wait times. Built Docker-based FAISS indexing solutions and used sentence-transformers for enhanced search accuracy.`,
+    Duration: "Jun 2022 - Aug 2022",
   },
 ];
 
@@ -226,58 +234,80 @@ export const eduDetails = [
     Location: "Austin, USA",
     Type: "Full Time",
     Duration: "May 2025",
-    Description: `Relevant Coursework: Advanced Data Mining, Big Data, Unstructured Data Analytics.`,
   },
   {
-    Position: "Bachelor of Technology, Computer Science & Engineering",
+    Position: "Bachelor of Technology, Computer Science and Engineering",
     Company: "PES University",
     Location: "Bangalore, India",
     Type: "Full Time",
     Duration: "May 2024",
-    Description: `Relevant Coursework: Database Systems, Data Structures, Cloud Technologies.`,
   },
 ];
 
 // Tech Stack and Tools
 export const techStackDetails = {
-  python: python,
-  java: java,
   html: html,
   css: css,
   js: js,
-  docker: docker,
-  spark: spark,
-  tableau: tableau,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
   git: git,
+  github: github,
+  figma: figma,
 };
 
 // Enter your Project Details here
 export const projectDetails = [
   {
-    title: "Real-Time QoS Analytics for Streaming Video Services",
-    description: `Developed a QoS solution leveraging Apache Spark on Databricks for real-time analytics, processing over 5 TB of data daily with Spark Streaming, MLlib, and Delta Lake.`,
-    techstack: "Apache Spark, Databricks, AWS Lambda",
-    previewLink: "",
-    githubLink: "",
-  },
-  {
     title: "ChoreoVision: Dance Visualization Tool",
-    description: `Created a Flask application to help choreographers visualize dance choreography, incorporating MFCC-based genre analysis and image synthesis using Stability API.`,
-    techstack: "Flask, Cohere API, Stability API",
-    previewLink: "",
-    githubLink: "",
+    image: projectImage1,
+    description: `A ReactJS and Flask application to help choreographers visualize dance choreography with suitable backgrounds and costumes, streamlining workflows and reducing manual effort by 30%. Incorporated music genre and emotion analysis for dynamic prompt generation and high-quality image synthesis using the Stability API.`,
+    techstack: "ReactJS, Flask, Cohere API, Stability API",
+    previewLink: "https://github.com/vidishachandra/choreovision",
+    githubLink: "https://github.com/vidishachandra/choreovision",
   },
   {
     title: "Resilient Distributed Data Streaming System",
-    description: `Designed a fault-tolerant data streaming system using socket programming, simulating a mini-Kafka architecture for efficient data distribution and high availability.`,
-    techstack: "Python, Socket Programming",
-    previewLink: "",
-    githubLink: "",
+    image: projectImage2,
+    description: `Designed a fault-tolerant data streaming system using Python socket programming, simulating a mini-Kafka architecture to optimize data ingestion and distribution. Implemented replication and high availability, handling up to 3 simultaneous broker failures.`,
+    techstack: "Python, Kafka, Zookeeper",
+    previewLink: "https://github.com/vidishachandra/data-streaming",
+    githubLink: "https://github.com/vidishachandra/data-streaming",
+  },
+  {
+    title: "NatyaSet: Classical Dance Dataset for Bharatanatyam",
+    image: projectImage3,
+    description: `Created a Bharatanatyam dance dataset featuring 2D and 3D pose data, facilitating enhanced data analysis for dance research. Implemented data extraction techniques using OpenPose, SMPL, and MediaPipe key points, integrating audio features from over 200 YouTube clips (600-1000 frames each) for a holistic gold-tier dataset.`,
+    techstack: "Python, OpenPose, MediaPipe, SMPL",
+    previewLink: "https://github.com/vidishachandra/natyaset",
+    githubLink: "https://github.com/vidishachandra/natyaset",
+  },
+  {
+    title: "Crime Against Women: An Analysis",
+    image: projectImage4,
+    description: `Performed exploratory data analysis and predicted the number of crimes reported and subsequent cases convicted with a machine learning model (Random Forest) achieving a normalized RMSE of 0.0111. Extracted data from NCRB reports and supplemented with governmental/non-governmental publications.`,
+    techstack: "Python, Random Forest, EDA",
+    previewLink: "https://github.com/vidishachandra/crime-analysis",
+    githubLink: "https://github.com/vidishachandra/crime-analysis",
+  },
+  {
+    title: "Blood-Bank Management System",
+    image: projectImage5,
+    description: `Developed a software system to effectively manage blood bank operations. Features include a central repository for blood deposit details, graphical representations of blood type availability, and user dashboards. Implemented using Python (Tkinter) for the interface and MySQL for database management.`,
+    techstack: "Python, Tkinter, MySQL",
+    previewLink: "https://github.com/vidishachandra/blood-bank",
+    githubLink: "https://github.com/vidishachandra/blood-bank",
   },
 ];
 
 // Enter your Contact Details here
 export const contactDetails = {
   email: "vidishachandra@utexas.edu",
-  phone: "(737) 781-4503",
+  phone: "+1 737 781 4503",
 };
