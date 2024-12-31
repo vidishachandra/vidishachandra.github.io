fc96004a93c62814589985c9d3a65da758cae683
vidishachandra <vidishasateesh@gmail.com>
2024-12-30 19:10:03 -0800

    Updated portfolio details_3

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,3 +1,56 @@
+// import React from "react";
+// import Work from "../Components/Work";
+// import { personalDetails, workDetails, eduDetails } from "../Details";
+
+// function About() {
+//   return (
+//     <main className="container mx-auto max-width pt-10 pb-20 ">
+//       <section>
+//         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+//           About Me
+//         </h1>
+//         <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
+//       </section>
+//       <section>
+//         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+//           Work Experience
+//         </h1>
+//         {React.Children.toArray(
+//           workDetails.map(({ Position, Company, Location, Type, Duration }) => (
+//             <Work
+//               position={Position}
+//               company={Company}
+//               location={Location}
+//               type={Type}
+//               duration={Duration}
+//             />
+//           ))
+//         )}
+//       </section>
+//       <section>
+//         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+//           Education
+//         </h1>
+//         {React.Children.toArray(
+//           eduDetails.map(({ Position, Company, Location, Type, Duration }) => (
+//             <Work
+//               position={Position}
+//               company={Company}
+//               location={Location}
+//               type={Type}
+//               duration={Duration}
+//             />
+//           ))
+//         )}
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default About;
+
+
+
 import React from "react";
 import Work from "../Components/Work";
 import { personalDetails, workDetails, eduDetails } from "../Details";
@@ -16,14 +69,17 @@ function About() {
           Work Experience
         </h1>
         {React.Children.toArray(
-          workDetails.map(({ Position, Company, Location, Type, Duration }) => (
-            <Work
-              position={Position}
-              company={Company}
-              location={Location}
-              type={Type}
-              duration={Duration}
-            />
+          workDetails.map(({ Position, Company, Location, Type, Duration, Description }) => (
+            <div className="mb-6">
+              <Work
+                position={Position}
+                company={Company}
+                location={Location}
+                type={Type}
+                duration={Duration}
+              />
+              <p className="text-content mt-2">{Description}</p>
+            </div>
           ))
         )}
       </section>
