1f0066a72aef99007993f08fbedad2557e61373a
vidishachandra <vidishasateesh@gmail.com>
2024-12-30 22:04:33 -0800

    Updated portfolio details_11

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,15 +6,12 @@ function Footer() {
         Designed and Coded by{" "}
         <a
           className="font-medium"
-          href="https://pavanmg.in"
-          target="_blank"
-          rel="noreferrer noopener"
+          // href="https://vidishachandra.github.io/"
+          // target="_blank"
+          // rel="noreferrer noopener"
         >
-          Pavan MG
-        </a>{" "}
-        with
-        <span className="text-gradient font-medium"> Love</span> &
-        <span className="text-gradient font-medium"> Coffee</span>
+          Vidisha Chandra
+        </a>
       </p>
     </footer>
   );
