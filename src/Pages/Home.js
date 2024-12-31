2b22b933b63d0f6ae0a79e0e05e3b680f5830be6
vidishachandra <vidishasateesh@gmail.com>
2024-12-30 22:17:25 -0800

    Updated portfolio details_15

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,93 @@
+// import React, { useRef, useEffect } from "react";
+// import gsap from "gsap";
+// import { personalDetails } from "../Details";
+
+// function Home() {
+//   const { name, tagline, img } = personalDetails;
+//   const h11 = useRef();
+//   const h12 = useRef();
+//   const h13 = useRef();
+//   const myimageref = useRef();
+//   useEffect(() => {
+//     const tl = gsap.timeline();
+//     tl.from(
+//       h11.current,
+//       {
+//         x: "-100%",
+//         delay: 0.8,
+//         opacity: 0,
+//         duration: 2,
+//         ease: "Power3.easeOut",
+//       },
+//       "<"
+//     )
+//       .from(
+//         h12.current,
+//         {
+//           x: "-100%",
+//           delay: 0.5,
+//           opacity: 0,
+//           duration: 2,
+//           ease: "Power3.easeOut",
+//         },
+//         "<"
+//       )
+//       .from(
+//         h13.current,
+//         {
+//           x: "-100%",
+//           delay: 0.1,
+//           opacity: 0,
+//           duration: 2,
+//           ease: "Power3.easeOut",
+//         },
+//         "<"
+//       )
+//       .from(
+//         myimageref.current,
+//         {
+//           x: "200%",
+//           delay: 0.5,
+//           opacity: 0,
+//           duration: 2,
+//           ease: "Power3.easeOut",
+//         },
+//         "<"
+//       );
+//   }, []);
+
+//   return (
+//     <main className="container mx-auto max-width section md:flex justify-between items-center">
+//       <div>
+//         <h1
+//           ref={h11}
+//           className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+//         >
+//           Hello!,👋<br></br>I'm<br></br>
+//         </h1>
+//         <h1
+//           ref={h12}
+//           className="text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+//         >
+//           {name}
+//         </h1>
+//         <h5
+//           ref={h13}
+//           className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+//         >
+//           {tagline}
+//         </h5>
+//       </div>
+//       <div className="mt-5 md:mt-0">
+//         <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Vidisha Chandra" />
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default Home;
+
+
 import React, { useRef, useEffect } from "react";
 import gsap from "gsap";
 import { personalDetails } from "../Details";
@@ -71,9 +161,10 @@ function Home() {
         >
           {name}
         </h1>
+        <div className="mt-4"></div> {/* Added blank line */}
         <h5
           ref={h13}
-          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+          className="text-lg text-dark-heading dark:text-light-heading md:text-2xl xl:text-3xl xl:leading-tight font-medium"
         >
           {tagline}
         </h5>
